7154f214418d07889370c8db2d1c8808de48c249
ilakkiya <ilakkiya.pugazhendi@acenture.com>
2020-02-24 17:33:03 +0530

    santhilatha

--- src/test/java/cs1/Runner.java
+++ src/test/java/cs1/Runner.java
@@ -3,7 +3,8 @@ package cs1;
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
-
+import io.cucumber.junit.CucumberOptions;
+@CucumberOptions(monochrome=true,plugin= {"pretty","json:cucumber-reports.json"})
 @RunWith(Cucumber.class)
 public class Runner {
 	
